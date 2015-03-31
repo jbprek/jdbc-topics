63b9cd78130d29321cfe9d10fdfa0c579d0abc4d
John V. Prekezes <john@prekezes.com>
2015-03-31 23:40:40 +0300

    updated SQL file

--- jdbc-playground/sql/schema.sql
+++ jdbc-playground/sql/schema.sql
@@ -13,3 +13,43 @@ WITH (
 );
 ALTER TABLE pair
   OWNER TO various;
+
+
+-- Table: master
+
+DROP TABLE master;
+
+CREATE TABLE master
+(
+  id serial NOT NULL,
+  value character varying(255) NOT NULL,
+  CONSTRAINT master_pkey PRIMARY KEY (id),
+  CONSTRAINT unique_id UNIQUE (id)
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE master
+OWNER TO various;
+
+-- Table: detail
+
+DROP TABLE detail;
+
+CREATE TABLE detail
+(
+  id serial NOT NULL,
+  value integer NOT NULL DEFAULT 0,
+  master_id integer NOT NULL,
+  CONSTRAINT detail_pkey PRIMARY KEY (id),
+  CONSTRAINT detail_master_id_fkey FOREIGN KEY (master_id)
+  REFERENCES master (id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+OIDS=FALSE
+);
+ALTER TABLE detail
+OWNER TO various;
+
+
